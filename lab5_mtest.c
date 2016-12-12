8fe63dc0fbbbcbbf2ea20cb1d03bf08c72675f8f
KoreaTech-System Programming 2016 <gkgkgy@koreatech.ac.kr>
2016-12-12 14:52:07 +0900

    Create lab5_mtest.c

---
+++ lab5_mtest.c
@@ -0,0 +1,13 @@
+#include"mtest.h"
+#include<stdio.h>
+#include<stdlib.h>
+extern int InFoo(), InBoo(), InBar();
+int main()
+{	
+	InFoo();
+	InBoo();
+	InBar();
+	printf("And this is Main Function.\n");
+	printf("Hello World!\n");
+	exit(0);
+}
